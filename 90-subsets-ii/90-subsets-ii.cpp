b2d5b9a5bb6ad527a416dd83af66bf68dd377fc5
Md. Mahinur Alam Ononno <64003114+mahinuralam@users.noreply.github.com>
2022-02-14 18:04:31 +0600

    Time: 4 ms (70.56%), Space: 16.7 MB (16.09%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector< vector<int> >ans;
+    
+    void backtrack(int i, vector<int>&nums, vector<int>tmp ){
+        
+        if(i == nums.size()){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        tmp.push_back(nums[i]);
+        
+        backtrack(i+1,nums,tmp);
+        
+        tmp.pop_back();
+        
+        while(i+1<nums.size()&&nums[i]==nums[i+1]){
+            i+=1;
+        }
+        
+        backtrack(i+1,nums,tmp);
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        ans.clear();
+        vector<int>tmp;
+        sort(nums.begin(),nums.end());
+        backtrack(0,nums,tmp);
+        
+        return ans;
+    }
+    
+};
